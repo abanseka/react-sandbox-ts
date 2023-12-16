874992fa8a3901507f0a1399a2359e435651f697
sly <sylvester@archis.company>
2023-12-16 07:10:45 +0100

    redux fundamentals

--- src/sandbox/Redux/Redux.tsx
+++ src/sandbox/Redux/Redux.tsx
@@ -1,5 +1,29 @@
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "./state/store";
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+} from "./state/counter/counterSlice";
+
 function Redux() {
-  return <div>Redux</div>;
+  const count = useSelector((state: RootState) => state.value);
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <h3>Redux</h3>
+      <h2>{count}</h2>
+
+      <div style={{ display: "flex", gap: 10 }}>
+        <button onClick={() => dispatch(increment())}>increment</button>
+        <button onClick={() => dispatch(decrement())}>decrement</button>
+        <button onClick={() => dispatch(incrementByAmount(2))}>
+          increment by 2
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default Redux;
